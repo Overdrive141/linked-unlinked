6ee7f2e8f744b1f7b296018e311800b4f5b546b5
Farhan <farhan.hammad@gmail.com>
2021-01-08 23:31:40 +0500

    Initial Commit

--- linked.js
+++ linked.js
@@ -1,1 +1,16 @@
 // find all the users that have not been linked
+import { allUsers, linked } from "./users";
+
+const checkLinked = (arr1, arr2) => {
+  let newArray = [];
+
+  newArray = arr1.filter((user1) => {
+    return !arr2.find((user2) => {
+      return user2.name === user1.name;
+    });
+  });
+
+  return newArray;
+};
+
+console.log(checkLinked(allUsers, linked));
